717a6d2f5f019ba6373e1d4769ac57635418876d
Fariz Ammar <f.ammarsyq11@gmail.com>
2024-03-15 06:29:31 +0700

    New Prototype

--- routes/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-
-// Dashboard
-Route::get('/dashboard', function () {
-    return view('pages.admin.dashboard');
-})->name('dashboard');
--- routes/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Authentication\AuthenticationController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('guest')->group(function () {
-    // Register View
-    Route::get('/', [AuthenticationController::class, 'registration'])->name('auth.register');
-});
